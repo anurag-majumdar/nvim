d809c00e990f89af6b3722b6b917ab91eccf52b9
anurag-majumdar <anurag.majumdar92@gmail.com>
2025-02-08 17:42:52 +0530

    Update debugger

--- lua/configs/dap.lua
+++ lua/configs/dap.lua
@@ -1,23 +1,74 @@
 local dap = require("dap")
 
+local js_based_languages = {
+  "typescript",
+  "javascript",
+  "typescriptreact",
+  "javascriptreact",
+  "vue"
+}
+
 dap.adapters["pwa-node"] = {
   type = "server",
-  host = "127.0.0.1",
-  port = 8123,
+  host = "localhost",
+  port = "${port}",
   executable = {
-    command = "js-debug-adapter",
+    command = "node",
+    -- 💀 Make sure to update this path to point to your installation
+    args = {"../../../js-debug-dap-v1.97.0/js-debug/src/dapDebugServer.js", "${port}"},
   }
 }
 
-for _, language in ipairs { "typescript", "javascript" } do
+-- dap.adapters["pwa-node"] = {
+--   type = "server",
+--   host = "127.0.0.1",
+--   port = 8123,
+--   executable = {
+--     command = "js-debug-adapter",
+--   }
+-- }
+
+for _, language in ipairs(js_based_languages) do
   dap.configurations[language] = {
+    -- Debug single nodejs files
     {
       type = "pwa-node",
       request = "launch",
-      name = "Launch file",
+      name = "Launch JS file",
       program = "${file}",
       cwd = "${workspaceFolder}",
-      runtimeExecutable = "node"
-    }
+    },
+    -- Debug nodejs process (make sure to add --inspect when you run the process)
+    {
+      type = "pwa-node",
+      request = "attach",
+      name = "Attach",
+      processId = require("dap.utils").pick_process,
+      cwd = "${workspaceFolder}",
+      sourceMaps = true,
+    },
   }
 end
+
+-- dap.configurations["javascript"] = {
+--   {
+--     type = "pwa-node",
+--     request = "launch",
+--     name = "Launch JS file",
+--     program = "${file}",
+--     cwd = "${workspaceFolder}",
+--     runtimeExecutable = "node",
+--   }
+-- }
+--
+-- dap.configurations["typescript"] = {
+--   {
+--     type = "pwa-node",
+--     request = "launch",
+--     name = "Launch JS file",
+--     program = "${file}",
+--     cwd = "${workspaceFolder}",
+--     runtimeExecutable = "node",
+--     sourceMaps = true,
+--   }
+-- }
